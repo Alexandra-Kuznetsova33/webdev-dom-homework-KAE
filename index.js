df23d07051d251266fe8d0bec29cffd165a470f3
Alexandra Kuznetsova <kuznetzovaschura@yandex.ru>
2026-04-10 20:09:07 +0300

    Добавление API в проект "комменты"

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-import { renderComments } from "/modules/renderComments.js";
+import { renderComments } from "./modules/renderComments.js";
 import {
   initLikeListeners,
   initReplyListeners,
   initAddCommentListener,
 } from "/modules/initListeners.js";
-import { commentsData } from "/modules/comments.js";
+import { commentsData } from "./modules/comments.js";
+import { fetchComments } from "./modules/api.js";
+import { updateComments } from "./modules/comments.js";
+const commentsList = document.querySelector(".comments");
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const commentsList = document.querySelector(".comments");
 
   if (commentsList) {
+    const serverComments = await fetchComments();
+    updateComments(serverComments);
     renderComments(commentsData, commentsList);
 
     initLikeListeners();
